9cf1e3a3f452643e9af22d5d18e876c69f1960e4
Aditya Sharma <pvtadityaaa@gmail.com>
2026-08-25 14:43:08 +0530

    build rms norm and encoder block

--- model.py
+++ model.py
@@ -14,7 +14,7 @@ class ModelArgs:
     vocab_size: int = -1 #this will be set when we load the tokenizer
     multiple_of=256
     ffn_dim_multiplier: Optional[float] = None
-    norm_e = 1e-5
+    norm_eps= 1e-5
 
     #needed for kv cache
     max_batch_size: int = 32
@@ -39,8 +39,58 @@ def precompute_thetha_pos_frequencies(head_dim: int, seq_len:int, device: str, t
     freqs_complex = torch.polar(torch.ones_like(freqs),freqs)
     return freqs_complex
 
-def apply_rotary_embeddings(x: torch.Tensor)
+def apply_rotary_embeddings(x: torch.Tensor,freqs_complex: torch.Tensor, device:str):
+    # (B,sew_len, h, head_dim)->(B,seq_len,h,head_dim/2)
+    x_complex=torch.view_as_complex(x.float().reshape(*x.shape[:-1],-1,2))
+    # (seq_len, head_Dim/2)->(1,seq_len,1,head_dim/2)
+    freqs_complex=freqs_complex.unsqueeze(0).unsqueeze(2)
+    # (B,seq_len,H,head_dim/2)*(1,seq_len,1,head_dim/2)->(B,seq_len,h,head_dim/2)
+    x_rotated=x_complex*freqs_complex
+    #(b,seq_len,h,head_dim/2)->(b,seq_len,h,head_dim/2,2)
+    x_out=torch.view_as_real(x_rotated)
+    #(b,seq_len,h,head_dim/2,2)->(B,seq_len,h,head_dim)
+    x_out=x_out.reshape(*x.shape)
+    return x_out.type_as(x).to(device)
 
+class RMSNorm(nn.Module):
+
+    def __init__(self,dim:int,eps: float = 1e-6):
+        super().__init__()
+        self.eps=eps
+        #gamma parameter
+        self.weight=nn.Parameter(torch.ones_like(dim))
+
+    def _norm(self,x:torch.Tensor):
+        #(B,seq_lenmdim)*(B,seq_len,1)=(B,seq_lendim)
+        return x*torch.rsqrt(x.pow(2).mean(-1,keepdim=True)+self.eps)
+
+    def forward(self,x:torch.Tensor):
+        #(dim)*(B,seq_len,Dim)=(B,seq_len,dim)
+        return self.weight*self._norm(x.float()).type_as(x)
+
+class EncoderBlock(nn.Module):
+
+    def __init__(self,args:ModelArgs):
+        super().__init__()
+
+        self.n_heads=args.n_heads
+        self.dim=args.dim
+        self.head_dim=args.dim//args.n_heads
+
+        self.attention=SelfAttention(args)
+        self.feed_forward=FeedForward(args)
+        #normalisation before self attention
+        self.attention_norm=RMSNorm(args.dim,eps=args.norm_eps)
+        #normalisation before feed forward block
+        self.ffn_norm=RMSNorm(args.dim,eps=args.norm_eps)
+
+    def forward(self, x:torch.Tensor, start_pos:int, freqs_complex: torch.Tensor):
+        #(B,seq_len,dim)+(B,swq_len,dim)->(B,seq_len,dim)
+        h=x+self.attention.forward(self.attention_norm(x),start_pos,freqs_complex)
+        out=h+self.feed_forward.forward(self.ffn_norm(h))
+        return out
+
+    
 class Transformer(nn.Module):
 
     def __init__(self, args: ModelArgs)->None:
