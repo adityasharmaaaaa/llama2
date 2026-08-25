94f13bb0c3976b1113ab1cfbfabd558b7e41bb21
Aditya Sharma <pvtadityaaa@gmail.com>
2026-08-25 15:14:36 +0530

    written self attemtion

--- model.py
+++ model.py
@@ -68,6 +68,89 @@ class RMSNorm(nn.Module):
         #(dim)*(B,seq_len,Dim)=(B,seq_len,dim)
         return self.weight*self._norm(x.float()).type_as(x)
 
+def repeat_kv(x:torch.Tensor,n_rep:int)->torch.Tensor:
+    batch_size,seq_len,n_kv_heads,head_dim=x.shape
+    if n_rep==1:
+        return x
+    return(
+        #(B,seq_len,N_KV_heads,1,head_dim)
+        x[:,:,:,None,:]
+        .expand(batch_size,seq_len,n_kv_heads,n_rep,head_dim)
+        .reshape(batch_size,seq_len,n_kv_heads*n_rep,head_dim)
+    )
+
+
+class SelfAttention(nn.Module):
+
+    def __init__(self,args:ModelArgs):
+        super().__init__()
+
+        #indicates the number of heads for key and values
+        self.n_kv_heads=args.n_heads if args.n_kv_heads is None else args.n_kv_heads
+        #indicates the number of heads for queries
+        self.n_heads_q=args.n_heads
+        #indicates how many times the heads of keys and values should be repeated to match the head of the queries
+        self.n_rep=self.n_heads_q//self.n_kv_heads
+        #indicates the dimension of each head
+        self.head_dim=args.dim//args.n_heads
+
+        self.wq=nn.Linear(args.dim,args.n_heads*self.head_dim,bias=False)
+        self.wk=nn.Linear(args.dim,self.n_kv_heads*self.head_dim,bias=False)
+        self.wv=nn.Linear(args.dim,self.n_kv_heads*self.head_dim,bias=False)
+        self.wo=nn.Linear(args.n_heads*self.head_dim,args.dim,bias=False)
+
+        self.cache_k=torch.zeros((args.max_batch_size,args.max_seq_len,self.n_kv_heads,self.head_dim))
+        self.cache_v=torch.zeros((args.max_batch_size,args.max_seq_len,self.n_kv_heads,self.head_dim))
+
+    def forward(self,x:torch.Tensor,start_pos:int,freqs_complex:torch.Tensor):
+        batch_size,seq_len,_=x.shape #(B,1,dim)
+
+        #apply the Wq, Wk and Wv matrices to queries keys and values
+        #(B,1,dim) -> (B,1,H_Q*head_dim)
+        xq=self.wq(x)
+        #(B,1,dim)->(B,1,M_KV*head_dim)
+        xk=self.wk(x)
+        xv=self.wv(x)
+
+        #(B,1,H_Q*head_dim)-->(B,1,H_Q,head_dim)
+        xq=xq.view(batch_size,seq_len,self.n_heads_q,self.head_dim)
+        xk=xk.view(batch_size,seq_len,self.n_kv_heads,self.head_dim)
+        #(B,1,H_KV*head_dim)-->(B,1J_KV,head_dim)
+        xv=xv.view(batch_size,seq_len,self.n_kv_heads,self.head_dim)
+
+        xq=apply_rotary_embeddings(xq,freqs_complex,device=x.device)
+        xk=apply_rotary_embeddings(xk,freqs_complex,device=x.device)
+ 
+        #replace the entry in the cache for this token
+        self.cache_k[:batch_size,start_pos:start_pos+seq_len]=xk
+        self.cache_v[:batch_size,start_pos:start_pos+seq_len]=xv
+
+        #retrieve all the cached keys and values so far
+        #(B,seq_len_KV,H_KV,head_dim)
+        keys=self.cache_k[:batch_size,0:start_pos+seq_len]
+        values=self.cache_v[:batch_size,0:start_pos+seq_len]
+
+        #repeat the heads of k and v to reach the number of heads of the queries
+        keys=repeat_kv(keys,self.n_rep) 
+        values=repeat_kv(values,self.n_rep)
+
+        #(B,1,H_q,head_dim)-->(B,H_q,1,head_dim)
+        xq=xq.trace(1,2)
+        keys=keys.transpose(1,2)
+        values=values.transpose(2,1)
+
+        #(B,H_Q,1,head_dim)@(B,H_q,head_dim,seq_len_kv)-->(B,H_Q,1,seq_len_kv)
+        scores=torch.matmul(xq,keys.transpose(2,3))//math.sqrt(self.head_dim)
+        scores=F.softmax(scores.float(),dim=-1).type_as(xq)
+
+        #(B,H_Q,1,seq_len)@(B,H_Q,seq_len_kv,head_dim)-->(B,H_Q,1,Head_Dim)
+        output=torch.matmul(scores,values)
+
+        #(B,H_Q,1,head_dim)->(B,1,H_Q,head_dim)-->(B,1,Dim)
+        output=(output.transpose(1,2).contiguous().view(batch_size,seq_len,-1))
+        return self.wo(output) #(B,1,dim)->(B,1,dim)
+
+
 class EncoderBlock(nn.Module):
 
     def __init__(self,args:ModelArgs):
